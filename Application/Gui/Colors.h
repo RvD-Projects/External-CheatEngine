cd753641bf23d1fa07419aed36d533b7ebeab65c
Raphael Vigneault Ð. <triangle1232009@hotmail.com>
2024-12-29 15:02:52 -0500

    Added some base colors

--- Application/Gui/Colors.h
+++ Application/Gui/Colors.h
@@ -12,26 +12,36 @@ const GuiColor Black{0, 0, 0, 255};
 const GuiColor Black12{0, 0, 0, 32.0f};
 const GuiColor Black25{0, 0, 0, 64.0f};
 const GuiColor Black50{0, 0, 0, 128.0f};
+const GuiColor Black64{0, 0, 0, 160.0f};
+const GuiColor Black75{0, 0, 0, 192.0f};
 
 const GuiColor White{255, 255, 255, 255};
 const GuiColor White12{255, 255, 255, 32.0f};
 const GuiColor White25{255, 255, 255, 64.0f};
 const GuiColor White50{255, 255, 255, 128.0f};
+const GuiColor White64{255, 255, 255, 160.0f};
+const GuiColor White75{255, 255, 255, 192.0f};
 
 const GuiColor Red{255, 0, 0, 255};
 const GuiColor Red12{255, 0, 0, 32.0f};
 const GuiColor Red25{255, 0, 0, 64.0f};
 const GuiColor Red50{255, 0, 0, 128.0f};
+const GuiColor Red64{255, 0, 0, 160.0f};
+const GuiColor Red75{255, 0, 0, 192.0f};
 
 const GuiColor Green{0, 255, 0, 255};
 const GuiColor Green12{0, 255, 0, 32.0f};
 const GuiColor Green25{0, 255, 0, 64.0f};
 const GuiColor Green50{0, 255, 0, 128.0f};
+const GuiColor Green64{0, 255, 0, 160.0f};
+const GuiColor Green75{0, 255, 0, 192.0f};
 
 const GuiColor Blue{0, 0, 255, 255};
 const GuiColor Blue12{0, 0, 255, 32.0f};
 const GuiColor Blue25{0, 0, 255, 64.0f};
 const GuiColor Blue50{0, 0, 255, 128.0f};
+const GuiColor Blue64{0, 0, 255, 160.0f};
+const GuiColor Blue75{0, 0, 255, 192.0f};
 
 const GuiColor GameRed{199, 51, 59, 255};
 const GuiColor GameGreen{56, 109, 41, 255};
