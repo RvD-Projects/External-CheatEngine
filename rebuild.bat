e0b414d50f376914e4ecc1e882ed16503843a688
Raphael Vigneault Ð. <triangle1232009@hotmail.com>
2025-01-02 14:27:06 -0500

    rebuild

--- rebuild.bat
+++ rebuild.bat
@@ -1,7 +1,8 @@
-cd Engine/GameDumper
+cd Engine\GameDumper
 call dump.bat
 
-cd ../
-cd ../
+cd ..
+cd ..
 
 call msbuild Application.sln /p:Configuration=Release /t:Rebuild
+call Build\Release\Application.exe
