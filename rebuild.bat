b286591490d0cbe084a4770b53512c1e4f5429e4
Raphael Vigneault Ð. <triangle1232009@hotmail.com>
2025-08-01 10:27:41 -0400

    ...

--- rebuild.bat
+++ rebuild.bat
@@ -1,6 +1,3 @@
-#rmdir /S /Q Engine\GameDumper\Dumps
-#call Engine\GameDumper\dump.bat
-
 rmdir /S /Q Build
 call msbuild Application.sln /p:Configuration=Release /t:Rebuild
 call Build\Release\Application.exe
